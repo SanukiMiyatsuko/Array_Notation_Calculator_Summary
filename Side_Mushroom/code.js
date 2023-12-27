17ccf16fda5237b5b06af095043edfb27d13f58a
SanukiMiyatsuko <132545189+SanukiMiyatsuko@users.noreply.github.com>
2023-12-27 17:39:31 +0900

    Update code.js

--- Side_Mushroom/code.js
+++ Side_Mushroom/code.js
@@ -1,52 +1,42 @@
 "use strict";
-//const katex = require('katex');
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 // DOM操作用の諸々
-var ABBR_SMALL_OMEGA = false;
-var ABBR_LARGE_OMEGA = false;
+let ABBR_SMALL_OMEGA = false;
+let ABBR_LARGE_OMEGA = false;
 let TO_TEX = false;
-window.onload = function (e) {
-    var small_omega = document.getElementById("small-omega");
-    var large_omega = document.getElementById("large-omega");
+window.onload = (e) => {
+    const small_omega = document.getElementById("small-omega");
+    const large_omega = document.getElementById("large-omega");
     const to_tex = document.getElementById("to-tex");
     if (small_omega)
-        small_omega.addEventListener("click", function () {
+        small_omega.addEventListener("click", () => {
             ABBR_SMALL_OMEGA = !ABBR_SMALL_OMEGA;
         });
     if (large_omega)
-        large_omega.addEventListener("click", function () {
+        large_omega.addEventListener("click", () => {
             ABBR_LARGE_OMEGA = !ABBR_LARGE_OMEGA;
         });
-    if(to_tex) 
+    if(to_tex)
         to_tex.addEventListener("click", () => {
             TO_TEX = !TO_TEX;
         });
 };
 function toggle_options() {
-    var r = document.getElementById("options");
+    const r = document.getElementById("options");
     if (!r)
         throw new Error("要素がないよ");
     r.style.display =
         (r.style.display == "none") ? "block" : "none";
 }
 function compute_fund() {
-    var str = document.getElementById("str");
-    var num = document.getElementById("num");
-    var output = document.getElementById("output");
+    const str = document.getElementById("str");
+    const num = document.getElementById("num");
+    const output = document.getElementById("output");
     if (!str || !num || !output)
         throw Error("要素がなかったよ");
-    var text = "";
+    let text = "";
     try {
-        var x = string_to_term(sanitize_string(str.value));
-        var y = string_to_term(sanitize_string(num.value));
+        const x = string_to_term(sanitize_string(str.value));
+        const y = string_to_term(sanitize_string(num.value));
         text = abbrviate(term_to_string(fund(x, y)));
     }
     catch (error) {
@@ -56,19 +46,19 @@ function compute_fund() {
     if(TO_TEX) {
         katex.render(text, output);
     } else {
-        output.innerHTML = text;
+        output.innerText = text;
     }
 }
 function compute_lt() {
-    var str = document.getElementById("str");
-    var num = document.getElementById("num");
-    var output = document.getElementById("output");
+    const str = document.getElementById("str");
+    const num = document.getElementById("num");
+    const output = document.getElementById("output");
     if (!str || !num || !output)
         throw Error("要素がなかったよ");
-    var text = "";
+    let text = "";
     try {
-        var x = string_to_term(sanitize_string(str.value));
-        var y = string_to_term(sanitize_string(num.value));
+        const x = string_to_term(sanitize_string(str.value));
+        const y = string_to_term(sanitize_string(num.value));
         text = less_than(x, y) ? "真" : "偽";
     }
     catch (error) {
@@ -78,17 +68,17 @@ function compute_lt() {
     if(TO_TEX) {
         katex.render(text, output);
     } else {
-        output.innerHTML = text;
+        output.innerText = text;
     }
 }
 function compute_dom() {
-    var str = document.getElementById("str");
-    var output = document.getElementById("output");
+    const str = document.getElementById("str");
+    const output = document.getElementById("output");
     if (!str || !output)
         throw Error("要素がなかったよ");
-    var text = "";
+    let text = "";
     try {
-        var x = string_to_term(sanitize_string(str.value));
+        const x = string_to_term(sanitize_string(str.value));
         text = abbrviate(term_to_string(dom(x)));
     }
     catch (error) {
@@ -98,12 +88,12 @@ function compute_dom() {
     if(TO_TEX) {
         katex.render(text, output);
     } else {
-        output.innerHTML = text;
+        output.innerText = text;
     }
 }
-var Z = { type: "zero" };
-var ONE = { type: "psi", sub: Z, arg: Z };
-var OMEGA = { type: "psi", sub: Z, arg: ONE };
+const Z = { type: "zero" };
+const ONE = { type: "psi", sub: Z, arg: Z };
+const OMEGA = { type: "psi", sub: Z, arg: ONE };
 // オブジェクトの相等判定（クソが代斉唱）
 // みんなはlodashとか使おう！
 function equal(s, t) {
@@ -115,7 +105,7 @@ function equal(s, t) {
             return false;
         if (t.arr.length != s.arr.length)
             return false;
-        for (var i = 0; i < t.arr.length; i++) {
+        for (let i = 0; i < t.arr.length; i++) {
             if (!equal(s.arr[i], t.arr[i]))
                 return false;
         }
@@ -143,7 +133,7 @@ function plus(a, b) {
             return { type: "plus", arr: [a, b] };
         }
         else {
-            return { type: "plus", arr: __spreadArray([a], b.arr, true) };
+            return { type: "plus", arr: [a, ...b.arr] };
         }
     }
     else {
@@ -151,7 +141,7 @@ function plus(a, b) {
             return a;
         }
         else if (b.type == "psi") {
-            return { type: "plus", arr: __spreadArray(__spreadArray([], a.arr, true), [b], false) };
+            return { type: "plus", arr: [...a.arr, b] };
         }
         else {
             return { type: "plus", arr: a.arr.concat(b.arr) };
@@ -192,8 +182,8 @@ function less_than(s, t) {
             return less_than(s.arr[0], t);
         }
         else {
-            var s2 = sanitize_plus_term(s.arr.slice(1));
-            var t2 = sanitize_plus_term(t.arr.slice(1));
+            const s2 = sanitize_plus_term(s.arr.slice(1));
+            const t2 = sanitize_plus_term(t.arr.slice(1));
             return less_than(s.arr[0], t.arr[0]) ||
                 (equal(s.arr[0], t.arr[0]) && less_than(s2, t2));
         }
@@ -208,8 +198,8 @@ function dom(t) {
         return dom(t.arr[t.arr.length - 1]);
     }
     else {
-        var domsub = dom(t.sub);
-        var domarg = dom(t.arg);
+        const domsub = dom(t.sub);
+        const domarg = dom(t.arg);
         if (equal(domarg, Z)) {
             if (equal(domsub, Z) || equal(domsub, ONE))
                 return t;
@@ -223,7 +213,7 @@ function dom(t) {
                 return domarg;
             if (domarg.type != "psi")
                 throw Error("そうはならんやろ");
-            var domargarg = dom(domarg.arg);
+            const domargarg = dom(domarg.arg);
             if (less_than(domargarg, domarg)) {
                 if (equal(domarg.sub, plus(t.sub, ONE)))
                     return t;
@@ -241,15 +231,15 @@ function find_parent(s, t) {
         return Z;
     }
     else if (s.type == "plus") {
-        var sub = s.arr[0].sub;
-        var remnant = sanitize_plus_term(s.arr.slice(1));
-        if (equal(sub, t))
+        const sub = s.arr[0].sub;
+        const remnant = sanitize_plus_term(s.arr.slice(1));
+        if (sub == t)
             return s;
         return find_parent(remnant, t);
     }
     else {
-        var sub = s.sub;
-        var arg = s.arg;
+        const sub = s.sub;
+        const arg = s.arg;
         if (equal(sub, t))
             return s;
         return find_parent(arg, t);
@@ -261,15 +251,15 @@ function fund(x, y) {
         return Z;
     }
     else if (x.type == "plus") {
-        var lastfund = fund(x.arr[x.arr.length - 1], y);
-        var remains = sanitize_plus_term(x.arr.slice(0, x.arr.length - 1));
+        const lastfund = fund(x.arr[x.arr.length - 1], y);
+        const remains = sanitize_plus_term(x.arr.slice(0, x.arr.length - 1));
         return plus(remains, lastfund);
     }
     else {
-        var sub = x.sub;
-        var arg = x.arg;
-        var domsub = dom(sub);
-        var domarg = dom(arg);
+        const sub = x.sub;
+        const arg = x.arg;
+        const domsub = dom(sub);
+        const domarg = dom(arg);
         if (equal(domarg, Z)) {
             if (equal(domsub, Z)) {
                 return Z;
@@ -296,16 +286,16 @@ function fund(x, y) {
             else {
                 if (domarg.type != "psi")
                     throw Error("なんでだよ");
-                var domargarg = dom(domarg.arg);
+                const domargarg = dom(domarg.arg);
                 if (domargarg.type == "zero") {
-                    var c = domarg.sub;
+                    const c = domarg.sub;
                     if (equal(c, plus(sub, ONE)))
                         return psi(sub, fund(arg, y));
                     if (equal(dom(y), ONE)) {
-                        var p = fund(x, fund(y, Z));
+                        const p = fund(x, fund(y, Z));
                         if (p.type != "psi")
                             throw Error("なんでだよ");
-                        var gamma = p.arg;
+                        const gamma = p.arg;
                         return psi(sub, fund(arg, psi(fund(c, Z), gamma)));
                     }
                     else {
@@ -313,13 +303,13 @@ function fund(x, y) {
                     }
                 }
                 else {
-                    var e = domargarg.sub;
+                    const e = domargarg.sub;
                     if (equal(e, plus(sub, ONE))) {
                         if (equal(dom(y), ONE)) {
-                            var p = fund(x, fund(y, Z));
+                            const p = fund(x, fund(y, Z));
                             if (p.type != "psi")
                                 throw Error("なんでだよ");
-                            var gamma = p.arg;
+                            const gamma = p.arg;
                             return psi(sub, fund(arg, find_parent(gamma, sub)));
                         }
                         else {
@@ -328,10 +318,10 @@ function fund(x, y) {
                     }
                     else {
                         if (equal(dom(y), ONE)) {
-                            var p = fund(x, fund(y, Z));
+                            const p = fund(x, fund(y, Z));
                             if (p.type != "psi")
                                 throw Error("なんでだよ");
-                            var gamma = p.arg;
+                            const gamma = p.arg;
                             return psi(sub, fund(arg, psi(fund(e, Z), gamma)));
                         }
                         else {
@@ -359,13 +349,13 @@ function term_to_string(t) {
 function abbrviate(str) {
     str = str.replace(/茸_\{0\}\(0\)/g, "1");
     while (true) {
-        var numterm = str.match(/1(\+1)+/);
+        const numterm = str.match(/1(\+1)+/);
         if (!numterm)
             break;
-        var matches = numterm[0].match(/1/g);
+        const matches = numterm[0].match(/1/g);
         if (!matches)
             throw ("そんなことある？");
-        var count = matches.length;
+        const count = matches.length;
         str = str.replace(numterm[0], count.toString());
     }
     if (ABBR_SMALL_OMEGA)
@@ -375,25 +365,23 @@ function abbrviate(str) {
     if(TO_TEX) str = to_TeX(str);
     return str;
 }
-
 function to_TeX(str) {
     str = str.replace(/茸/g, "\\textrm{茸}");
     str = str.replace(/ω/g, "\\omega");
     str = str.replace(/Ω/g, "\\Omega");
     return str;
 }
-
 // ======================================
 // position以降のbeginはじまりend終わりの対応するカッコを探し，
 // その位置を返す
 function serach_closure(str, position, begin, end) {
-    var count = 0;
-    var pos = position;
+    let count = 0;
+    let pos = position;
     while (str[pos] != begin && pos < str.length) {
         pos += 1;
     }
     for (; pos < str.length; pos += 1) {
-        var ch = str[pos];
+        const ch = str[pos];
         if (ch == begin)
             count += 1;
         if (ch == end)
@@ -401,12 +389,12 @@ function serach_closure(str, position, begin, end) {
         if (count == 0)
             return pos;
     }
-    throw Error("Unclosed braces");
+    throw Error(`Unclosed braces`);
 }
 // 最も左のAP
 function leftmost_principal(str) {
-    var subpos = serach_closure(str, 0, "{", "}");
-    var argpos = serach_closure(str, subpos + 1, "(", ")");
+    const subpos = serach_closure(str, 0, "{", "}");
+    const argpos = serach_closure(str, subpos + 1, "(", ")");
     return str.substring(0, argpos + 1);
 }
 function string_to_term(str) {
@@ -414,49 +402,49 @@ function string_to_term(str) {
         throw Error("Empty string");
     if (str == "0")
         return Z;
-    var left = leftmost_principal(str);
-    var leftlen = left.length;
+    const left = leftmost_principal(str);
+    const leftlen = left.length;
     if (left == str)
         return principal_string_to_term(left);
-    var remains = str.slice(leftlen + 1);
+    const remains = str.slice(leftlen + 1);
     return plus(principal_string_to_term(left), string_to_term(remains));
 }
 function principal_string_to_term(str) {
     if (str.length == 0)
-        throw Error("Empty principal term");
-    var position = 0;
+        throw Error(`Empty principal term`);
+    let position = 0;
     if (position >= str.length)
         throw Error("PT成分をTermにできなかったよ");
-    var ch = str[position];
+    const ch = str[position];
     if (ch != "茸" && ch != "m")
-        throw Error("Unexpected token '".concat(ch, "' in AP element"));
+        throw Error(`Unexpected token '${ch}' in AP element`);
     position += 1;
-    var ch2 = str[position];
+    const ch2 = str[position];
     if (ch2 != "_")
-        throw Error("Unexpected token '".concat(ch2, "' after \u8338 or p"));
+        throw Error(`Unexpected token '${ch2}' after 茸 or m`);
     position += 1;
-    var ch3 = str[position];
+    const ch3 = str[position];
     if (ch3 != "{")
-        throw Error("Invalid subscript ".concat(ch3));
-    var subpos = serach_closure(str, position, "{", "}");
-    var sub = str.substring(position + 1, subpos);
+        throw Error(`Invalid subscript ${ch3}`);
+    const subpos = serach_closure(str, position, "{", "}");
+    const sub = str.substring(position + 1, subpos);
     position = subpos + 1;
-    var ch4 = str[position];
+    const ch4 = str[position];
     if (ch4 != "(")
-        throw Error("Invalid subscript ".concat(ch4));
-    var argpos = serach_closure(str, position, "(", ")");
-    var arg = str.substring(position + 1, argpos);
+        throw Error(`Invalid subscript ${ch4}`);
+    const argpos = serach_closure(str, position, "(", ")");
+    const arg = str.substring(position + 1, argpos);
     return psi(string_to_term(sub), string_to_term(arg));
 }
 function sanitize_string(str) {
     str = str.replace(/\s/g, "");
     while (true) {
-        var numstr = str.match(/[1-9][0-9]*/);
+        const numstr = str.match(/[1-9][0-9]*/);
         if (!numstr)
             break;
-        var num = parseInt(numstr[0]);
-        var numterm = "m_{0}(0)";
-        for (var i = 1; i < num; i++) {
+        const num = parseInt(numstr[0]);
+        let numterm = "m_{0}(0)";
+        for (let i = 1; i < num; i++) {
             numterm += "+m_{0}(0)";
         }
         str = str.replace(numstr[0], numterm);
